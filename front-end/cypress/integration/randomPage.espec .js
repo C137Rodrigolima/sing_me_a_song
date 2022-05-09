6a812243bdb315ad8bad095bfbdb3e9e6187e5e0
Carlos Rodrigo <rodrigolimac137@gmail.com>
2022-05-09 16:28:54 -0300

    Fix: test E2E-RandomPage

--- front-end/cypress/integration/randomPage.espec .js
+++ front-end/cypress/integration/randomPage.espec .js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
-describe("Top Page", ()=>{
-  it("Should return mone music by random", ()=>{
-    cy.visit("http://localhost:3000/top");
+describe("Random Page", ()=>{
+  it("Should return one music by random", ()=>{
+    cy.visit("http://localhost:3000/random");
 
     cy.intercept("GET", "http://localhost:5000/recommendations/random").as("getRandomRecommendation");
     cy.wait("@getRandomRecommendation");
