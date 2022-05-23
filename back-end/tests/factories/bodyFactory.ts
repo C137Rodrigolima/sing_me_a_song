a0d393fa3e7f8a2dcf7de2b0a7b1cfefb478e6a1
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-05-23 05:10:58 -0300

    Preparando revisão de código

--- back-end/tests/factories/bodyFactory.ts
+++
@@ -1,9 +0,0 @@
-import { faker } from "@faker-js/faker";
-import { CreateRecommendationData } from "../../src/services/recommendationsService";
-
-export default function bodyFactory() : CreateRecommendationData {
-  return {
-    name: faker.lorem.word(3),
-    youtubeLink: "https://www.youtube.com/watch?v=4VxdufqB9zg",
-  }
-}
